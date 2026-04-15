9f2c4dba7f851264955ae7a2796f7a44156aacbd
Mablr <59505383+mablr@users.noreply.github.com>
2026-04-15 16:28:44 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use reth_ethereum::{
+    node::{
+        builder::{components::NoopNetworkBuilder, NodeBuilder, NodeHandle},
+        core::{args::RpcServerArgs, node_config::NodeConfig},
+        node::EthereumAddOns,
+        EthereumNode,
+    },
+    tasks::Runtime,
+};
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    let runtime = Runtime::test();
+    let node_config = NodeConfig::test()
+        .dev()
+        .with_rpc(RpcServerArgs::default().with_http());
+    let NodeHandle {
+        node,
+        node_exit_future,
+    } = NodeBuilder::new(node_config)
+        .testing_node(runtime)
+        .with_types::<EthereumNode>()
+        .with_components(EthereumNode::components().network(NoopNetworkBuilder::eth()))
+        .with_add_ons(EthereumAddOns::default())
+        .launch()
+        .await?;
+
+    println!(
+        "anvil-reth dev node started on {:?}",
+        node.rpc_server_handles.rpc.http_local_addr()
+    );
+
+    node_exit_future.await?;
+
+    Ok(())
+}
